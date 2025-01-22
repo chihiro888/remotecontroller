d0402d3cd773274487284b6803ec860d94375f06
chihiro888 <b930208@gmail.com>
2025-01-22 22:39:34 +0900

    feat: test chakra ui

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,10 @@
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
 const Home = () => {
   return (
     <>
       <h1>hello</h1>
+      <Button colorScheme="blue">Button</Button>
     </>
   )
 }
